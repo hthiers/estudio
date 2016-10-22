fb1bd210ac7ca297ad8cb126a1ba2e7b52d692b6
fethe <fethe15@gmail.com>
2016-10-22 00:34:30 -0300

    Borrados directorios sin usar

--- resources/views/assets/scripts.blade.php
+++ resources/views/assets/scripts.blade.php
@@ -10,15 +10,9 @@
 <![endif]-->
 
 {{-- Jquery --}}
-<script   src="http://code.jquery.com/jquery-2.2.4.min.js"
-          integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
-          crossorigin="anonymous">
-</script>
+<script src="{{ url('js/jquery.min.js') }}"></script>
 {{-- Boostrap --}}
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-        crossorigin="anonymous">
-</script>
+<script src="{{ url('js/bootstrap.min.js') }}"></script>
 {{-- Gentelella --}}
 <script src="{{ url('js/gentelella.min.js') }}"></script>
 {{-- Js propio--}}
